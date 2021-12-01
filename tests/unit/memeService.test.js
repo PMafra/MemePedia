b79b6fa631e59c002796e3c08d4a93156527cf2d
PMafra <pmafrarm@gmail.com>
2021-12-01 11:42:11 -0300

    Finish unit tests for listMemes function in memeService

---
+++ tests/unit/memeService.test.js
@@ -0,0 +1,48 @@
+import Filter from 'bad-words';
+import * as memeService from '../../src/services/memeService.js';
+import * as memeRepository from '../../src/repository/memeRepository.js';
+import * as userRepository from '../../src/repository/userRepository.js';
+
+const sut = memeService;
+jest.mock('bad-words');
+
+describe('memeService test', () => {
+    it('Should return no memes object for limit = 0 ', async () => {
+        const result = await sut.listMemes(0);
+        expect(result).toEqual({
+            message: 'No memes today!',
+            data: []
+        });
+    });
+
+    it('Should return no memes object for limit = -1 ', async () => {
+        const result = await sut.listMemes(-1);
+        expect(result).toEqual({
+            message: 'No memes today!',
+            data: []
+        });
+    });
+
+    it('Should return no memes object for limit = 1 and empty list of memes', async () => {
+        jest.spyOn(memeRepository, 'listMemes').mockImplementationOnce(() => {
+            return []
+        });
+        const result = await sut.listMemes(1);
+        expect(result).toEqual({
+            message: 'No memes today!',
+            data: []
+        });
+    });
+
+    it('Should return list of memes object for limit = 1 and existing memes', async () => {
+        const memes = ['meme1', 'meme2', 'meme3'];
+        jest.spyOn(memeRepository, 'listMemes').mockImplementationOnce(() => {
+            return memes;
+        });
+        const result = await sut.listMemes(1);
+        expect(result).toEqual({
+            message: 'List all memes',
+            data: memes,
+        });
+    });
+});
